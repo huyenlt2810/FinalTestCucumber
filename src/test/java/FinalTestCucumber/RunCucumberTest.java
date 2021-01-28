d618b031054295c2b67020e23f1f1c0ee945e506
huyenlt2810 <67570812+huyenlt2810@users.noreply.github.com>
2021-01-28 09:21:47 +0700

    Update RunCucumberTest.java

--- src/test/java/FinalTestCucumber/RunCucumberTest.java
+++ src/test/java/FinalTestCucumber/RunCucumberTest.java
@@ -5,7 +5,7 @@ import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(plugin = {"pretty", "html:target/report.html"}, glue = "stepDefinition")
+@CucumberOptions(plugin = {"pretty", "html:target/report.html", "json:target/report.json"}, glue = "stepDefinition")
 public class RunCucumberTest {
 
 }
